2f7838a04731565187d187e5904350fbb373bb45
wei <mavenwei@qq.com>
2015-11-07 12:58:22 +0800

    edit shell script

--- daily.sh
+++ daily.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
-dataStr=$(date -v -1d +%m%d)
+#以当天日期为备注提交日志
+if [ "$1" == "" ];then message=$(date -v -1d +%m%d)
+else message="$1"
+fi
 git add .
-git ci "$dataStr"
+git ci "$message"
 git push origin master
